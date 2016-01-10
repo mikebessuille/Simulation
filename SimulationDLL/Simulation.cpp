ef1f7d1857f95156e4011c4977ce7f7bc384a0f3
Mike Bessuille <mikebessuille@hotmail.com>
2016-01-09 22:27:35 -0500

    Updated from Henry

--- SimulationDLL/Simulation.cpp
+++ SimulationDLL/Simulation.cpp
@@ -55,7 +55,7 @@ void Simulation::Loop()
 	std::cout << "\n";
 }
 
-/* Happy Face annimation:
+/* Happy Face annimation: (From Henry)
 std::cout << "\b\b\b:)" << std::flush;
 std::this_thread::sleep_for(std::chrono::milliseconds(100));
 std::cout << "\b\b'_'" << std::flush;
