80c1fd3c2aafb0cbd9ee4ff693840059f331f2cd
Mike Bessuille <mikebessuille@hotmail.com>
2016-01-12 18:41:36 -0500

    Updated unit tests!  No, actually, really, I did!

--- UnitTestProject/ThreadTest.cpp
+++ UnitTestProject/ThreadTest.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include <thread>
 #include "Simulation.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace UnitTestProject
 {		
@@ -15,6 +17,23 @@ namespace UnitTestProject
 		{
 			Simulation sim;
 			sim.Start();
+			sim.Stop();
+			Assert::IsFalse(sim.IsRunning());
+		}
+
+		TEST_METHOD(TestSimulationStartStop)
+		{
+			Simulation sim;
+			Assert::IsFalse(sim.IsRunning());
+			sim.Start();
+			Assert::IsTrue(sim.IsRunning());
+			this_thread::sleep_for(chrono::seconds(3));
+			Assert::IsTrue(sim.IsRunning());
+			sim.Stop();
+			this_thread::sleep_for(chrono::seconds(1));
+			sim.Start();
+			this_thread::sleep_for(chrono::seconds(2));
+			Assert::IsTrue(sim.IsRunning());
 			sim.Stop();
 			Assert::IsFalse(sim.IsRunning());
 		}
