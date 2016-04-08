26e9d626f8ed06df5f4e5b88694698ce24cc73fd
Mike Bessuille <mbessuille@klocwork.com>
2016-04-08 13:37:55 -0700

    Refactored the UnitMgr test class.  started trying to fix the way units are destroyed from that list.

--- UnitTestProject/UnitMgrTest.cpp
+++ UnitTestProject/UnitMgrTest.cpp
@@ -19,34 +19,22 @@ namespace UnitTestProject
 
 			// Assert::IsNotNull();
 		}
-
-		TEST_METHOD(TestUnitMgrList)
+		
+		
+		void PopulateList(UnitMgr UM)
 		{
-			UnitMgr UM;
-			Assert::AreEqual(UM.NumUnits(), 0);
-
 			for (int i = 0; i < 10; i++)
 			{
 				UM.AddUnit(new ExampleUnit());
-				Assert::AreEqual(UM.NumUnits(), i+1);
+				Assert::AreEqual(UM.NumUnits(), i + 1);
 			}
-		
-			/* // This doesn't work... exception in Vector class.
-			for (int pos = UM.NumUnits(); pos > 0; pos-- )
-			{
-				int nOldSize = UM.NumUnits();
-				UnitBase *pUnit = UM.unitList.at(pos);
-				UM.DestroyUnit(pos, pUnit );
+		}
 
-			}
-			*/
-
-
-			for (auto &unit : UM.unitList)
-			{
-				//unit->...();
-			}
-			
+		void DestroyList(UnitMgr UM)
+		{
+			/*
+			// This is a terrible way to destroy the list... and it leaks the actual objects!
+			// (It's a list of pointers; this just removes the pointers but doesn't free what is being pointed to)
 			auto it = UM.unitList.begin();
 			while (it != UM.unitList.end())
 			{
@@ -56,7 +44,47 @@ namespace UnitTestProject
 				Assert::AreNotEqual(nOldSize, nNewSize);
 				// How can we verify that the memory actually got deleted?
 			}
+			*/
+
+			for (auto unit : UM.unitList)
+			{
+				int nOldSize = UM.NumUnits();
+				// TODO:  Not sure why this fails to compile!
+				delete (UnitBase)*unit;	// Delete the unit that is pointed to
+				unit = UM.unitList.erase(unit);	// Remove it from the Vector
+				int nNewSize = UM.NumUnits();
+				Assert::AreNotEqual(nOldSize, nNewSize);
+			}
+		}
+
+		void DoSomethingWithList(UnitMgr UM)
+		{
+			for (auto &unit : UM.unitList)
+			{
+				//unit->...();
+			}
+		}
+
+		TEST_METHOD(TestUnitMgrList)
+		{
+			UnitMgr UM;
+			Assert::AreEqual(UM.NumUnits(), 0);
+
+			PopulateList(UM);
+			DoSomethingWithList(UM);
+			DestroyList(UM);
 		}
 
 	};
 }
+
+
+/* // This doesn't work... exception in Vector class.
+for (int pos = UM.NumUnits(); pos > 0; pos-- )
+{
+int nOldSize = UM.NumUnits();
+UnitBase *pUnit = UM.unitList.at(pos);
+UM.DestroyUnit(pos, pUnit );
+
+}
+*/
