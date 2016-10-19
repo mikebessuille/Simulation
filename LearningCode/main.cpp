07a2e7935ba395bfd2be758485b87097719546ae
Mike Bessuille <mike.bessuille@roguewave.com>
2016-10-19 12:35:48 -0400

    Simple implementation so user can hit any key, not just Enter.

--- LearningCode/main.cpp
+++ LearningCode/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <conio.h>
+#include <thread>
 
 #include "CPP11Features.h"
 
@@ -7,12 +9,17 @@ using namespace std;
 int main()
 {
 	cout << "Learning Code Project.  Hit Enter to start:" << endl;
-	std::cin.get(); // gets any user input (requires user to hit enter as well)
+	cin.get(); // gets any user input (requires user to hit enter as well)
 
 
 	CPP11Features cpp11;
 	cpp11.run();
 
-	cout << "Learning Code Project Complete.  Hit Enter to end." << endl;
-	std::cin.get(); // gets any user input (requires user to hit enter as well)
+	// Get any char as input.
+	cout << "Learning Code Project Complete.  Hit any key to end." << endl;
+	while (_kbhit() == 0) {}
+	char ch = _getch();
+	cout << "Char: " << ch << endl; // if we used _getch() directly here, it would output the ASCII (int) value, not the char.
+	this_thread::sleep_for(chrono::seconds(3));
+	return(true);
 }
