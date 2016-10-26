8976109c5dd648b03a699e995db368699bb8d3a6
Mike Bessuille <mikebessuille@hotmail.com>
2016-10-26 15:51:51 -0400

    Dunno why these were changed on Deathstar...

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Day 1,Breakfast: I had cereal wich made me sick. Then the doorbell rang." << endl;
+    return 0;
+}
+
