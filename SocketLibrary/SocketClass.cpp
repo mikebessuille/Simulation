e63af153150c72c993803178b56a1f33b177ea6b
Mike Bessuille <mike.bessuille@roguewave.com>
2017-05-15 17:10:24 -0400

    Next todo comment

--- SocketLibrary/SocketClass.cpp
+++ SocketLibrary/SocketClass.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 
+//TODO:  Separate server and client code into subclasses or superclasses of this basic socket design; 
+// A socket should just be a single connection to one other computer.
+// The server should have a list of sockets, one for each client.
+
 SocketClass::SocketClass( bool bIsServer ) : bServer( bIsServer )
 {
 	int result;
